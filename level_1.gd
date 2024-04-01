b037520f50b05e549baf0ce1eb76d6619e51475c
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-04-01 18:15:19 -0300

    Increased the number of enemies in scene one will be working on updating player controls next.

--- level_1.gd
+++ level_1.gd
@@ -7,22 +7,26 @@ extends Node3D
 var pickupsLeft = 0
 const PICKUPS_TO_WIN = 5
 
+var spawnPoints: Array[Marker3D]
 var walls: Array[SceneTree]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	spawnPoints = [ $EnemySpawnPoint_1, $EnemySpawnPoint_2, $EnemySpawnPoint_3, $EnemySpawnPoint_4 ]
 	#_generate_maze()
 	var p = player.instantiate()
 	p.position.x = $StartPoint.position.x
 	p.position.z = $StartPoint.position.z
 	$Camera3D.player = p
 	add_child(p)
-	var e = enemy_1.instantiate()
-	e.player = p
-	e.position.x = $EnemySpawnPoint_1.position.x
-	e.position.z = $EnemySpawnPoint_1.position.z
-	add_child(e)
+	for sp in spawnPoints:
+		var e = enemy_1.instantiate()
+		e.player = p
+		e.position.x = sp.position.x
+		e.position.z = sp.position.z
+		add_child(e)
 
+# not really used any more
 func _generate_maze():
 	for i in 50:
 		var w = wall.instantiate()
