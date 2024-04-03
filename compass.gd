08793cf5c899c4b6c666cda9ddd7cea055aff227
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-04-03 14:58:01 -0300

    Got the compass workingish.

---
+++ compass.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@export var player: CharacterBody3D
+@export var enemies: Array[CharacterBody3D]
+@export var terminals: Array[PackedScene]
+
+var enemey_icon
+var multi_mesh
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	enemey_icon = $Godot_Icon
+	multi_mesh = $MultiMeshInstance2D.multimesh
+	multi_mesh.mesh = enemey_icon.mesh
+	multi_mesh.instance_count = 4
+	_place_marker()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _place_marker():
+	var screen_size = get_viewport_rect().size
+	var center = Vector2(screen_size.x/2,screen_size.y/2)
+	for i in multi_mesh.instance_count:
+		var v = Vector2(randi_range(-3,3),randi_range(-3,3))
+		v = v.normalized()
+		var position = center + v * 260
+		var skew = 0.0
+		var rotation = 0.0
+		var scale = Vector2(0.3,0.3)
+		var t = Transform2D(rotation, scale, skew, position)
+		multi_mesh.set_instance_transform_2d(i, t)
