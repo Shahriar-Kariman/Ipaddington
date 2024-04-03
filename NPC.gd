aceebaa701691ce078b9891e49aed8aa84a6c931
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-04-03 15:29:15 -0300

    I made the NPC move sorat.

--- NPC.gd
+++ NPC.gd
@@ -1,6 +1,9 @@
 extends CharacterBody3D
+
+@onready var nav: NavigationAgent3D = $NavigationAgent3D
+
 const speed = 5
-var current_state = IDLE
+var current_state = NEW_DIR
 var direction = Vector3.RIGHT
 var start_pos
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -16,27 +19,34 @@ func ready():
 	start_pos = position
 
 func _process(delta):
-	print(position)
-		
 	match current_state:
 		IDLE:
 			pass
 		NEW_DIR:
-			direction = choose([Vector3.RIGHT,Vector3.FORWARD, Vector3.BACK,Vector3.LEFT])
+			var target = Vector3(randf_range(-100,100),2,randf_range(-100,100))
+			nav.target_position = target
+			current_state = NEW_DIR
 		MOVE:
 			move(delta)
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -=gravity *delta
-		
+
 func move (delta):
-	position += direction *speed *delta 
+	direction = nav.get_next_path_position() - global_position
+	look_at(direction)
+	rotation.x = 0
+	rotation.z = 0
 	
-	#ADD Boundaries
-	#if position.x >= start_pos.x +20:
-	#	position.x =  start_pos.x +18
+	velocity += direction
+
+	velocity = velocity.normalized() * delta * speed
 	
+	position += velocity
+	
+	move_and_slide()
+
 func choose (array):
 	array.shuffle()
 	return array.front()
